544cc18ad31664e6939db32440f0d863a668db35
Krait <krait@skbkontur.ru>
2018-06-26 06:04:36 +0500

    An alternative implementation of CombinedSource.

--- Vostok.Configuration/Sources/CombinedSource.cs
+++ Vostok.Configuration/Sources/CombinedSource.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using JetBrains.Annotations;
 using Vostok.Configuration.Abstractions;
@@ -14,19 +12,14 @@ namespace Vostok.Configuration.Sources
     /// <summary>
     /// A source that combines settings from several other sources, resolving possible conflicts in favor of sources which one came earlier in the list
     /// </summary>
-    public class CombinedSource : IConfigurationSource, IDisposable // CR(krait): Are you sure this cannot be done a lot easier using Subject<T>?
+    public class CombinedSource : IConfigurationSource
     {
-        // CR(krait): What's the point of annotating private fields?
         [NotNull]
         private readonly IReadOnlyCollection<IConfigurationSource> sources;
         private readonly SettingsMergeOptions options;
-        private readonly IList<IObserver<ISettingsNode>> observers;
-        private readonly ConcurrentBag<IDisposable> watchers;
-        private readonly IDictionary<IConfigurationSource, ISettingsNode> sourcesSettings;
+        // CR(krait): What's the point of annotating private fields?
+        [NotNull]
         private readonly TaskSource taskSource;
-        private readonly object locker;
-        private ISettingsNode currentValue;
-        private bool neverMerged;
 
         /// <summary>
         /// <para>Creates a <see cref="CombinedSource"/> instance new source using combining options.</para>
@@ -40,12 +33,7 @@ namespace Vostok.Configuration.Sources
         {
             this.sources = sources;
             this.options = options;
-            neverMerged = true;
-            locker = new object();
             taskSource = new TaskSource();
-            watchers = new ConcurrentBag<IDisposable>();
-            observers = new List<IObserver<ISettingsNode>>();
-            sourcesSettings = new Dictionary<IConfigurationSource, ISettingsNode>();
         }
 
         /// <inheritdoc />
@@ -55,7 +43,7 @@ namespace Vostok.Configuration.Sources
         /// </summary>
         /// <param name="sources">Configurations</param>
         public CombinedSource(params IConfigurationSource[] sources)
-            : this(sources.ToArray(), new SettingsMergeOptions())
+            : this(sources, new SettingsMergeOptions())
         {
         }
 
@@ -72,74 +60,12 @@ namespace Vostok.Configuration.Sources
         /// <para>Returns current value immediately on subscribtion.</para>
         /// </summary>
         /// <returns>Event with new RawSettings tree</returns>
-        public IObservable<ISettingsNode> Observe() =>
-            Observable.Create<ISettingsNode>(
-                observer =>
-                {
-                    if (!watchers.Any())
-                    {
-                        SubscribeWatchers(observer);
-                        currentValue = Merge(sourcesSettings.Values);
-                    }
-
-                    if (!observers.Contains(observer))
-                        observers.Add(observer);
-                    observer.OnNext(currentValue);
-
-                    return Disposable.Create(
-                        () =>
-                        {
-                            if (observers.Contains(observer))
-                                observers.Remove(observer);
-                        });
-                });
-
-        public void Dispose()
+        public IObservable<ISettingsNode> Observe()
         {
-            foreach (var watcher in watchers)
-                watcher.Dispose();
-        }
+            if (!sources.Any())
+                return Observable.Return<ISettingsNode>(null);
 
-        private void SubscribeWatchers(IObserver<ISettingsNode> observer)
-        {
-            foreach (var source in sources)
-            {
-                var src = source;
-                sourcesSettings.Add(src, src.Get());
-                var watcher = source.Observe()
-                    .Subscribe(
-                        newSettings =>
-                        {
-                            lock (locker)
-                            {
-                                if (!Equals(newSettings, sourcesSettings[src]))
-                                {
-                                    sourcesSettings[src] = newSettings;
-                                    currentValue = Merge(sourcesSettings.Values);
-                                    observer.OnNext(currentValue);
-                                }
-
-                                if (neverMerged && currentValue != null)
-                                    observer.OnNext(currentValue);
-                            }
-                        });
-                watchers.Add(watcher);
-            }
-        }
-
-        private ISettingsNode Merge(IEnumerable<ISettingsNode> settingses)
-        {
-            neverMerged = false;
-
-            var sets = settingses as ISettingsNode[] ?? settingses.ToArray();
-            if (!sets.Any() || sets.Any(s => s == null)) return null;
-            if (sets.Length == 1) return sets[0];
-
-            var merge = sets[0];
-            for (var i = 1; i < sets.Length; i++)
-                merge = merge.Merge(sets[i], options);
-
-            return merge;
+            return sources.Select(s => s.Observe()).CombineLatest().Select(l => l.Aggregate((a, b) => a.Merge(b, options)));
         }
     }
 }
